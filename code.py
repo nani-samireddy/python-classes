007273048b88727bc1f223728619722356a2389a
nani-samireddy <nanisamireddy05@gmail.com>
2022-07-23 10:21:45 +0530

    added semiPrime

--- code.py
+++ code.py
@@ -1,30 +1,7 @@
 
 import math as M
+from funs import *
 
-def isPrime(n):
-    if(n==1):
-        return False
-    else:
-        isprime = True
-        end = int(M.sqrt(n))+1
-        for i in range(2,end):
-            if(n%i==0):
-                isprime=False
-                break
-        return isprime
-
-    
-def isSPrime(number):
-    primeCounter = 2
-    res=False
-    counter = int(M.sqrt(number))
-    for i in range(2,counter+1):
-        if number%i==0 and isPrime(i) and isPrime(number//i):
-            res=True
-            break
-    return res
-    
-        
     
 t = int(input())
 for _ in range(0,t):
--- funs.py
+++ funs.py
@@ -21,3 +21,28 @@ def isSpy(number):
         sum +=int(digit)
         prod *=int(digit)
     return sum==prod
+
+def isPrime(n):
+    if(n==1):
+        return False
+    else:
+        isprime = True
+        end = int(M.sqrt(n))+1
+        for i in range(2,end):
+            if(n%i==0):
+                isprime=False
+                break
+        return isprime
+
+    
+def isSPrime(number):
+    primeCounter = 2
+    res=False
+    counter = int(M.sqrt(number))
+    for i in range(2,counter+1):
+        if number%i==0 and isPrime(i) and isPrime(number//i):
+            res=True
+            break
+    return res
+    
+        
