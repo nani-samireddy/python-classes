ada81cd44d1e17321ffa4354b647728d96b1339a
nani-samireddy <nanisamireddy05@gmail.com>
2022-07-28 12:24:51 +0530

    day 4

--- code.py
+++ code.py
@@ -1,10 +1,26 @@
+import math
+sum=0
+t=2
+def lcmOfNumbers(a,b):
+    global t
+    res=1
+    if t>a or t>b:
+        return a*b
+    else:
+        if a%t==0 and b%t==0:
+            a=a//t
+            b=b//t
+            res=t
+        else:
+            t+=1
+        print(a,b,t)
+        return res * lcmOfNumbers(a//t,b//t)
+        
 
-import math as M
-from funs import *
 
+test = int(input())
+for _ in range(0,test):
+    a,b=map(int,input().split())
+    t=2
+    print(lcmOfNumbers(a,b))
     
-t = int(input())
-for _ in range(0,t):
-    num = int(input())
-    print(isSPrime(num))
-
