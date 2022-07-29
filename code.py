2845858ad548cbefb617371d84ead050d11dcc5b
nani-samireddy <nanisamireddy05@gmail.com>
2022-07-29 12:10:14 +0530

    day 5 end

--- code.py
+++ code.py
@@ -1,21 +1,28 @@
 import math
 
-def gcd(a,b):
-    res = 0
-    while(a>0 and b>0):
-        if(a>b):
-            a=a%b
-        else:
-            b=b%a
-    if a>b:
-        return a
-    else:
-        return b
-
-
-test = int(input())
-for _ in range(0,test):
-    a,b=map(int,input().split())
+def lcm(a,b):
     t=2
-    print("GCD is=",gcd(a,b))
+    res=1
+    while(a>=t and b>=t):
+        if(a%t==0 and b%t==0):
+            a=a//t
+            b=b//t
+            res=res*t
+        else:
+            t+=1        
+    return res*a*b
+
+def arrLCM(arr):
+    res=arr[0]
+    for i in range(1,len(arr)):
+        res=lcm(res,arr[i])
+    return res
+        
+
+
+n = int(input())
+arr = [0]*n
+for _ in range(0,n):
+    arr[_]=int(input())
+print(arrLCM(arr))
     
